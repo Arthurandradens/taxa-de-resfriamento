1de81c7a93aee0a983bd3cf67801598cc79bf84d
Ederson Moreira <114446581+EderDeev@users.noreply.github.com>
2023-12-12 20:22:48 -0300

    Update interface.py
    Código atualizado 12/12/2023

--- interface.py
+++ interface.py
@@ -2,45 +2,42 @@ from PySimpleGUI import Window, Button,Text, Image,Input, Column, HSeparator,Pus
 import matplotlib.pyplot as plt
 import numpy as np
 
-theme('darkblue9')
+theme('Reddit')
 
-def calcula_resfriamento(temperatura_ambiente,temperatura_inicial,constante_resfriamento,tempo_total,intervalo_tempo):
-    num_pontos = int(tempo_total / intervalo_tempo)
+def calcula_resfriamento(Ta,T,k,t):
+    lista_tempo = list(range(0,t))  # Cria uma lista de tempo que vai de acordo com o tempo(t)
+    lista_temperatura = np.zeros(t)  # cria uma lista de zeros que vai de acordo com o tempo(t)
+    lista_temperatura[0] = T  # Define que o primeiro valor da lista vai ser a temperatura inicial(T)
 
-    # Arrays para armazenar os valores do tempo e da temperatura
-    tempos = np.linspace(0, tempo_total, num_pontos)#vai gerar um array intervalo com base no tempo total / intervalo
-    temperaturas = np.zeros(num_pontos)#vai criar as temperaturas zeradas com base nos pontos
-    temperaturas[0] = temperatura_inicial #definir a temperatura inicial
+    i = 1  # Valor de inicio do loop
+    # Cálculo da derivada de newton que calcula a taxa de resfriamento de um objeto com base no tempo
+    while i < t:
+        lista_temperatura[i] = Ta + (T - Ta) * np.exp(-k * i)  # Converte o logaritmo para um número real
+        i += 1
 
-    # Calculando a temperatura para cada intervalo de tempo
-    for i in range(1, num_pontos):
-        derivada = constante_resfriamento * (temperaturas[0] - temperatura_ambiente) * np.exp(-constante_resfriamento * (tempos[i]))#Derivada do newton
-        temperaturas[i] = temperaturas[i-1] - derivada
-    # Plotando o gráfico
-    plt.figure(figsize=(10, 10))#Define o tamanho do gráfico
-    plt.plot(tempos, temperaturas, label='Temperatura do objeto')
-    plt.axhline(y=temperatura_ambiente, color='r', linestyle='--', label='Temperatura ambiente')
+    plt.figure(figsize=(10, 10))  # Define o tamanho do gráfico
+    plt.plot(lista_tempo, lista_temperatura, label='Temperatura do objeto')
+    plt.axhline(y=Ta, color='r', linestyle='--', label='Temperatura ambiente')
     plt.title('Resfriamento do objeto ao longo do tempo')
     plt.xlabel('Tempo (minutos)')
     plt.ylabel('Temperatura (°C)')
-    plt.legend()#Adiciona as descrições no gráfico
-    plt.grid(True)#Gerar as linhas
-    plt.show()
+    plt.legend()  # Adiciona as descrições no gráfico
+    plt.grid(True)  # Gerar as linhas
+    plt.show()  # Plota o gráfico
 
 
 layout_esquerda = [
-        [Text('********* Descubra a taxa de resfriamento do seu telhado *********')],
+        [Text(' Descubra a taxa de resfriamento do seu telhado ')],
         [Image(filename='telhado2.png')],
 ]
 
 layout_direita = [
 
     
-    [Text('Temperatura do ambiente(Graus):'),Input(key='temp_ambiente')],
-    [Text('Temperatura inicial do objeto:      '),Input(key='temp_inicial')],
-    [Text('Constante de resfriamento:         '),Input(key='constante_resfriamento')],
-    [Text('Tempo total:                              '),Input(key='tempo_total')],
-    [Text('Intervalo de tempo:                     '),Input(key='intervalo_tempo')],
+    [Text('Temperatura do ambiente(Graus):'),Input(key='Ta')],
+    [Text('Temperatura inicial do objeto:      '),Input(key='T')],
+    [Text('Constante de resfriamento:         '),Input(key='k')],
+    [Text('Tempo total:                              '),Input(key='t')],
     [Push(),Button('Calcular', size=(20,1)),Push()],
 ]
 
@@ -60,12 +57,11 @@ while True :
 
     match(event):
         case 'Calcular':
-                temperatura_ambiente = float(values['temp_ambiente'])
-                temperatura_inicial = float(values['temp_inicial'])
-                constante_resfriamento = float(values['constante_resfriamento'])
-                tempo_total = float(values['tempo_total'])
-                intervalo_tempo = float(values['intervalo_tempo'])
-                calcula_resfriamento(temperatura_ambiente,temperatura_inicial,constante_resfriamento,tempo_total,intervalo_tempo)
+                temperatura_ambiente = float(values['Ta'])
+                temperatura_inicial = float(values['T'])
+                constante_resfriamento = float(values['k'])
+                tempo_total = int(values['t'])
+                calcula_resfriamento(temperatura_ambiente,temperatura_inicial,constante_resfriamento,tempo_total)
                 
         case None:
               break    
